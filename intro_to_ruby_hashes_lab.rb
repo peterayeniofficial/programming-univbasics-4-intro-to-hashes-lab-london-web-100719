1b65c166c2cc043156cd78482d9d03270545a35f
Peter Ayeni <ayenicology@gmail.com>
2019-09-01 13:27:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,12 +5,12 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  return {name: "Peter", age: 12}
+  return {:name => "Peter", :age => 12}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  return {name: "Grace Hopper"}
+  return {:name => "Grace Hopper"}
 end
 
 def id_generator
