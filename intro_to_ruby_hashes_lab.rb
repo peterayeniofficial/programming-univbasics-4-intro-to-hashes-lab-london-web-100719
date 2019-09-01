c57a4f5955f0021659f911aa131cb456b38ddebf
Peter Ayeni <ayenicology@gmail.com>
2019-09-01 13:54:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,7 +33,10 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
   hash.each {|key, value|
-  if hash[:key]
+  if :key 
+    hash[:key] = 1
+  else 
+    hash[:key] = value += 1
   puts "#{key} is #{value}" }
   
   
